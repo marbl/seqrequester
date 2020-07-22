8994a247f55d3d63f5bc15f3f83e8d4ce672eb5b
Brian P. Walenz <thebri@gmail.com>
2020-07-22 16:47:27 -0400

    Remove unused crud from submakefiles.

--- src/seqrequester/seqrequester.mk
+++ src/seqrequester/seqrequester.mk
@@ -1,16 +1,3 @@
-
-#  If 'make' isn't run from the root directory, we need to set these to
-#  point to the upper level build directory.
-ifeq "$(strip ${BUILD_DIR})" ""
-  BUILD_DIR    := ../$(OSTYPE)-$(MACHINETYPE)/obj
-endif
-ifeq "$(strip ${TARGET_DIR})" ""
-  TARGET_DIR   := ../$(OSTYPE)-$(MACHINETYPE)
-endif
-
-#            shiftregister-emit-fast.C \
-#            shiftregister-emit-slow.C \
-
 TARGET   := seqrequester
 SOURCES  := seqrequester.C \
             extract.C \
