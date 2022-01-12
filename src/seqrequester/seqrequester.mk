25a2410635b5a6e07d4718f72e9a1f80bc660911
Brian P. Walenz <thebri@gmail.com>
2022-01-12 11:45:23 -0500

    Add support for submodule builds.

--- src/seqrequester/seqrequester.mk
+++ src/seqrequester/seqrequester.mk
@@ -12,7 +12,24 @@ SOURCES  := seqrequester.C \
             simulate.C \
             summarize.C
 
-SRC_INCDIRS  := . .. ../utility/src/utility
+SRC_INCDIRS  := .
+
+#  If we're part of Canu, build with canu support and use Canu's copy of
+#  meryl-utility.  Otherwise, don't.  (meryl does this too)
+ifneq ($(wildcard stores/sqStore.H), )
+  SRC_CXXFLAGS := -DCANU
+  SRC_INCDIRS  := ../../../utility/src/utility ../../../stores
+
+#  If we're part of something else, include the something else's
+#  utility directory.
+else ifneq ($(wildcard seqrequester/src/seqrequester/seqrequester.C), )
+  SRC_INCDIRS  := ../../../utility/src/utility
+
+#  Otherwise, we're building directly in the seqrequester repo.
+else
+  SRC_INCDIRS  := ../utility/src/utility
+
+endif
 
 TGT_LDFLAGS := -L${TARGET_DIR}/lib
 TGT_LDLIBS  := -l${MODULE}
