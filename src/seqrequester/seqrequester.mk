7e50fc1238ce0bf3996a5ba832f45a94bd356b2d
Brian P. Walenz <thebri@gmail.com>
2024-10-31 15:12:30 -0400

    Don't double link to compression libraries.

--- src/seqrequester/seqrequester.mk
+++ src/seqrequester/seqrequester.mk
@@ -32,7 +32,6 @@ else
 
 endif
 
-#  htslib needs the compressors and curl.
 TGT_LDFLAGS := -L${TARGET_DIR}/lib
-TGT_LDLIBS  := -l${MODULE} -lz -llzma -lbz2 -lcurl
+TGT_LDLIBS  := -l${MODULE}
 TGT_PREREQS := lib${MODULE}.a
